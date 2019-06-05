9a952826d8c2099fc8ea0cbf7dd4cd9e97bc6d1b
Yi Xuan Qi <47555540+yixuanQicq@users.noreply.github.com>
2019-06-05 13:20:50 -0700

    Added Comments For Talking Cat

--- talkingCat.py
+++ talkingCat.py
@@ -1,5 +1,10 @@
+# Get the input from the user
 text = input("What do you want the cat to say?")
+
+# Determine the length of the input
 text_length = len(text)
+
+# Make the border the same size as the input
 print('          {}'.format('_' * text_length))
 print('        < {} >'.format(text))
 print('          {}'.format('-' * text_length))
